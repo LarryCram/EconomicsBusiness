566a9eac776743a1b0ad799e843fdac8e1d6dc5a
Lawrence Cram <Lawrence.Cram@anu.edu.au>
2026-02-15 15:10:09 +1100

    better journal preprocessor

--- preprocessor/journal_preprocessor.py
+++ preprocessor/journal_preprocessor.py
@@ -1,15 +1,34 @@
+from pathlib import Path
 import duckdb
 import glob
 
 # 1. Find all xlsx files
-files = glob.glob('data/*.xlsx')
+DATA_PATH = Path('/home/lc/Projects/EconomicsBusiness/data/')
+print(f'{DATA_PATH.exists() = }')
+files = list(DATA_PATH.glob('WOS_JCR/*.xlsx'))
+print('{files = }')
 
 # 2. Create the table from the first file
-duckdb.sql(f"CREATE TABLE all_data AS SELECT * FROM read_xlsx('{files[0]}')")
+with duckdb.connect() as db:
+    file = files[0]
+    sql = f"""CREATE TABLE all_data AS SELECT * FROM read_xlsx('{file}', range="A3:F:999", header=true) WHERE "JCR Abbreviation" NOT NULL"""
+    db.sql(sql)
+    db.sql("SELECT * FROM all_data").show()
 
-# 3. Insert the rest
-for file in files[1:]:
-    duckdb.sql(f"INSERT INTO all_data SELECT * FROM read_xlsx('{file}')")
 
-# Verify
-duckdb.sql("SELECT count(*) FROM all_data").show()
+    # 3. Insert the rest
+    for file in files[1:]:
+        sql = f""" INSERT INTO all_data SELECT * FROM read_xlsx('{file}', range="A3:F:999", header=true) WHERE "JCR Abbreviation" NOT NULL"""
+        db.sql(sql)
+        db.sql("SELECT * FROM all_data").show()
+
+    # 4. Pack Categories into mulit-category journals
+    df = db.sql("""SELECT "Journal name" AS journal, ISSN, eISSN, list(Category) AS categories FROM all_data GROUP BY ALL""").df().drop_duplicates('journal')
+    print(f'{df.shape = }\n{df.head()}')
+
+    # 4. Save to combined journal spreadsheet
+    out_file = DATA_PATH/'incites_journal_list.csv'
+    sql = f"""COPY (
+                SELECT "Journal name" AS journal, ISSN, eISSN, list(Category) AS categories FROM all_data GROUP BY ALL
+                ) TO '{out_file}' (FORMAT csv)"""
+    db.sql(sql)
