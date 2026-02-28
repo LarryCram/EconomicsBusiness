dd1a9db516188a26fe74671bc25c295982d67ed9
Larry Cram <Lawrence.Cram@anu.edu.au>
2026-02-28 16:31:27 +1100

    sync

--- prepare_data/journal_filter_match_oa_load_works_aships.py
+++ prepare_data/journal_filter_match_oa_load_works_aships.py
@@ -32,7 +32,8 @@ def filter_and_match(db):
         ) TO '/home/lc/Projects/EconomicsBusiness/data/journal_list_matched.parquet' (FORMAT PARQUET); 
         """
     db.sql(sql)
-    sql = """        SELECT count(source_id) AS source_count, subfield
+    sql = """
+        SELECT count(source_id) AS source_count, subfield
         FROM (SELECT source_id, unnest(subfield_list) AS subfield
                 FROM read_parquet('/home/lc/Projects/EconomicsBusiness/data/journal_list_matched.parquet'))
         GROUP BY ALL
@@ -41,9 +42,37 @@ def filter_and_match(db):
     db.sql(sql).show()
     return
 
+def load_works_authorships(db):
+    sql = """
+        -- 1 LOAD works and authorships for the JOURNAL LIST
+        -- =================================================
+        SET preserve_insertion_order=FALSE;
+
+        COPY (
+        SELECT w.*
+                FROM read_parquet('/home/lc/m/working/econ_bus/parquet/sources.parquet')
+                JOIN (SELECT * FROM read_parquet('/home/lc/m/openalex_feb26/parquet/works/*.parquet')) w
+                USING (source_id)
+        ) TO '/home/lc/m/working/econ_bus/parquet/works.parquet' (FORMAT PARQUET);
+
+        COPY (
+        SELECT a.*
+            FROM read_parquet('/home/lc/m/working/econ_bus/parquet/works.parquet')
+            JOIN (SELECT * FROM read_parquet('/home/lc/m/openalex_feb26/parquet/authorships/*.parquet')) a
+            USING (work_id)
+        ) TO '/home/lc/m/working/econ_bus/parquet/authorships.parquet' (FORMAT PARQUET);
+    """
+    db.sql(sql)
+    db.sql("SELECT count(DISTINCT work_id) as works_count_econ_bus FROM '/home/lc/m/working/econ_bus/parquet/works.parquet'").show()
+    db.sql("SELECT count(DISTINCT source_id) as source_count_econ_bus FROM '/home/lc/m/working/econ_bus/parquet/works.parquet'").show()
+    db.sql("SELECT count(DISTINCT author_id) as author_count_econ_bus FROM '/home/lc/m/working/econ_bus/parquet/authorships.parquet'").show()
+    db.sql("SELECT count(DISTINCT institution_id) as institution_count_econ_bus FROM '/home/lc/m/working/econ_bus/parquet/authorships.parquet'").show()
+    return
+
 def main():
     with duckdb.connect() as db:
         filter_and_match(db)
+        load_works_authorships(db)
 
 if __name__ == "__main__":
     main()
